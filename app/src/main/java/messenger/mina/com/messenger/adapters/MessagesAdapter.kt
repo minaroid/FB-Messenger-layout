cbce0d2a4b0c8aa4e3dced33a8ec9cc42faa1153
mina george <m.george@intcore.com>
2019-05-03 18:54:13 +0200

    enhance design colors

--- app/src/main/java/messenger/mina/com/messenger/adapters/MessagesAdapter.kt
+++ app/src/main/java/messenger/mina/com/messenger/adapters/MessagesAdapter.kt
@@ -5,19 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.chauthai.swipereveallayout.ViewBinderHelper
 import kotlinx.android.synthetic.main.item_message.view.*
 import messenger.mina.com.messenger.R
 import messenger.mina.com.messenger.models.MessageModel
 
 class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
     private val messages: ArrayList<MessageModel> = ArrayList()
+    private val viewBinderHelper = ViewBinderHelper()
 
     init {
-        messages.add(MessageModel("Mina George", "Hi, mina", "10:35 PM", 2, true, true, "", false, false,R.drawable.user_3))
+        viewBinderHelper.setOpenOnlyOne(true)
+
+        messages.add(
+            MessageModel(
+                "Mina George",
+                "Hi, minaaaaa....",
+                "10:35 PM",
+                2,
+                true,
+                true,
+                "",
+                false,
+                false,
+                R.drawable.user_3
+            )
+        )
         messages.add(
             MessageModel(
                 "Ayman Maged",
-                "Hi, my best friend :)",
+                "Hi, my best friend :)...?",
                 "09:35 PM",
                 1,
                 false,
@@ -25,13 +42,13 @@ class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
                 "33m",
                 true,
                 true,
-                R.drawable.user_1
+                R.drawable.user_2
             )
         )
         messages.add(
             MessageModel(
                 "Mai George",
-                "Hi, Mai how are you ???",
+                "Hi, Mai how are you ?????.",
                 "20:45 PM",
                 2,
                 true,
@@ -45,40 +62,66 @@ class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
         messages.add(
             MessageModel(
                 "Mina George",
-                "Hi, what about your dream yeasterday ? ",
+                "Hi, what about your dream yeasterday ?..... ",
                 "12:35 PM",
                 1,
                 false,
                 true,
                 "",
-                false, false,R.drawable.user_4
+                false, false, R.drawable.user_4
             )
         )
-        messages.add(MessageModel("Mona Sameh", "Hi, mina", "10:35 PM", 2, false, true, "", false, true,R.drawable.user_2))
+        messages.add(
+            MessageModel(
+                "Mona Sameh",
+                "Hi, mina....",
+                "10:35 PM",
+                2,
+                false,
+                true,
+                "",
+                false,
+                true,
+                R.drawable.user_2
+            )
+        )
         messages.add(
             MessageModel(
                 "Ayman Maged",
-                "Hi, my best friend :)",
+                "Hi, my best friend :)...?",
                 "09:35 PM",
                 1,
                 false,
                 false,
                 "33m",
                 true,
-                false ,R.drawable.user_1
+                false, R.drawable.user_2
             )
         )
-        messages.add(MessageModel("Mai George", "Hi, mina how are you ???", "Thu", 2, true, false, "21m", true, true,R.drawable.user_4))
+        messages.add(
+            MessageModel(
+                "Mai George",
+                "Hi, mina how are you ???....",
+                "Thu",
+                2,
+                true,
+                false,
+                "21m",
+                true,
+                true,
+                R.drawable.user_4
+            )
+        )
         messages.add(
             MessageModel(
                 "Mina George",
-                "Hi, what about your dream yeasterday ? ",
+                "Hi, what about your dream yeasterday ?......... ",
                 "07 Jan",
                 1,
                 false,
                 true,
                 "",
-                false, false,R.drawable.user_2
+                false, false, R.drawable.user_2
             )
         )
     }
@@ -96,29 +139,25 @@ class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val image = view.cv_image
         private val name = view.tv_name
-        private val messege = view.tv_message
+        private val message = view.tv_message
         private val mute = view.im_mute
         private val newIndicator = view.view_new
         private val online = view.view_online
         private val time = view.view_time
-        private val messegeTime = view.tv_time
-        private val imageBackground = view.cv_background
+        private val messageTime = view.tv_time
         private val context = view.context
+        private val swipeRevealLayout = view.swipeRevealLayout
 
         fun bind(messageModel: MessageModel) {
+            // ToDO up must add the second param as unique id
+            viewBinderHelper.bind(swipeRevealLayout, messageModel.message)
             image.setImageResource(messageModel.image)
             name.text = messageModel.name
-            messegeTime.text = ". ${messageModel.time}"
-            if (messageModel.sender == 1)
-                if (messageModel.message.length > 15)
-                    messege.text = "You: ${messageModel.message.substring(0, 15)}"
-                else
-                    messege.text = "You: ${messageModel.message}"
-            else
-                if (messageModel.message.length > 15)
-                    messege.text = "${messageModel.message.substring(0, 15)}"
-                else
-                    messege.text = "${messageModel.message}"
+            messageTime.text = ". ${messageModel.time}"
+            when {
+                messageModel.sender == 1 -> message.text = "You: ${messageModel.message}"
+                else -> message.text = "${messageModel.message}"
+            }
             if (messageModel.isNew)
                 newIndicator.visibility = View.VISIBLE
             else
@@ -135,14 +174,14 @@ class MessagesAdapter : RecyclerView.Adapter<MessagesAdapter.ViewHolder>() {
                 time.visibility = View.VISIBLE
                 time.text = messageModel.onlineTime
             }
-            if(messageModel.isStory){
-                imageBackground.setImageResource(R.color.colorBlue)
+            if (messageModel.isStory) {
+                image.isActivated = true
             }
             if (messageModel.isNew) {
                 newIndicator.visibility = View.VISIBLE
                 name.setTypeface(null, Typeface.BOLD)
-                messege.setTypeface(null, Typeface.BOLD)
-                messege.setTextColor(context.resources.getColor(R.color.colorBlack))
+                message.setTypeface(null, Typeface.BOLD)
+                message.setTextColor(context.resources.getColor(R.color.ColorText))
             } else {
                 newIndicator.visibility = View.GONE
             }
